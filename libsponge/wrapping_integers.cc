f956f14804f6790cda02c28e0cf675175c18df8b
Gaoustcer <2337284889@qq.com>
2023-04-03 22:09:08 +0800

    unwrap v1

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,7 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    uint32_t num = n % (numeric_limits<uint32_t>::max() + 1);
+    uint32_t num = static_cast<uint32_t> (n & (UINT32_MAX));
     // DUMMY_CODE(n, isn);
     return WrappingInt32{num + isn.raw_value()};
 }
@@ -30,7 +30,31 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
+    // seqno -> absolute seqno
+    // to close to checkpoint in which range
     // DUMMY_CODE(n, isn, checkpoint);
-    // return isn - n;
-    return {}; // isn - n 
+    uint64_t index = 0;
+    if(n.raw_value() < isn.raw_value()){
+        index = static_cast<uint64_t>(UINT32_MAX + 1 + n.raw_value() - isn.raw_value()); 
+
+        // There must be an index rewind
+    }
+    else{
+        index = static_cast<uint64_t>(n.raw_value() - isn.raw_value());
+    }
+    uint64_t lowerbound;
+    uint64_t higherbound = ((checkpoint + 1) >> 32) << 32;
+    if((checkpoint >> 32) == 0){
+        lowerbound = 0;
+    }
+    else{
+        lowerbound = (((checkpoint + 1) >> 32) - 1) << 32;
+    }
+    if(index > (UINT32_MAX > 1)){
+        return {index + lowerbound};
+    }
+    else{
+        return {index + higherbound};
+    }
 }
+// UINT32_MAX
